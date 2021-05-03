f62462730acb7e8e381741bbd6b5c6a7213896f3
semih1239 <semihayyildiz23@hotmail.com>
2021-05-04 01:11:45 +0200

    Added lessons exercises

---
+++ javascript/javascript1/week1/exercises/exercises.js
@@ -0,0 +1,54 @@
+// var cross = 24 * 55;
+// console.log(cross);
+
+// const myName = "Semih";
+// console.log(myName);
+
+// console.log(myName[0]);
+
+// let firstArray = ["Hello", "World", "Hi",3,5,7,true,false,true];
+
+// console.log(firstArray[3]);
+
+// console.log(myName[myName.length-1]);
+
+// console.log(typeof 3);
+// console.log(typeof -33);
+// console.log(typeof '3');
+
+// const threeConst = 3;
+// console.log(threeConst);
+
+// let threeLet = 3;
+// console.log(threeLet);
+
+// console.log(typeof 'console.log("console.log(console.log(""))")');
+// console.log(typeof typeof 45);
+
+// const names = ['benjamin', 'Christopher'];
+// console.log(typeof names[0]);
+
+// console.log(typeof [3][0]);
+
+// console.log(typeof true);
+
+// console.log(1 == 1); //true
+// console.log(1 != 2); //true
+// console.log('HELLO' == 2); //false
+// console.log('1' == 1); //true
+// console.log('1' === 1); //false
+// console.log(1 > 8); //false
+// console.log(1 <= 8) //true
+// console.log(1 <= 1); //true
+
+// console.log(1 === 1 && 3 === 4);  //false
+// console.log((1 !== 1 || 3 === 4) || true );  //true
+
+let name = 'benjamin';
+name = 'benjamin-better';
+
+const pizzaPrice = 78;
+const pizzaPriceDiscounted = pizzaPrice - 10;
+
+const users = ['peter', 'Johnny', 'Børge'];
+const lastUser = users[users.length-1];

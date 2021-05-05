0d37a25e0e1bff7a09fea1b715435200db79fc89
semih1239 <semihayyildiz23@hotmail.com>
2021-05-05 17:58:48 +0200

    exercises is not my works, they are my lessons backup. Btw fixed comment lines

--- javascript/javascript1/week1/exercises/exercises.js
+++ javascript/javascript1/week1/exercises/exercises.js
@@ -1,48 +1,48 @@
-// var cross = 24 * 55;
-// console.log(cross);
+var cross = 24 * 55;
+console.log(cross);
 
-// const myName = "Semih";
-// console.log(myName);
+const myName = "Semih";
+console.log(myName);
 
-// console.log(myName[0]);
+console.log(myName[0]);
 
-// let firstArray = ["Hello", "World", "Hi",3,5,7,true,false,true];
+let firstArray = ["Hello", "World", "Hi",3,5,7,true,false,true];
 
-// console.log(firstArray[3]);
+console.log(firstArray[3]);
 
-// console.log(myName[myName.length-1]);
+console.log(myName[myName.length-1]);
 
-// console.log(typeof 3);
-// console.log(typeof -33);
-// console.log(typeof '3');
+console.log(typeof 3);
+console.log(typeof -33);
+console.log(typeof '3');
 
-// const threeConst = 3;
-// console.log(threeConst);
+const threeConst = 3;
+console.log(threeConst);
 
-// let threeLet = 3;
-// console.log(threeLet);
+let threeLet = 3;
+console.log(threeLet);
 
-// console.log(typeof 'console.log("console.log(console.log(""))")');
-// console.log(typeof typeof 45);
+console.log(typeof 'console.log("console.log(console.log(""))")');
+console.log(typeof typeof 45);
 
-// const names = ['benjamin', 'Christopher'];
-// console.log(typeof names[0]);
+const names = ['benjamin', 'Christopher'];
+console.log(typeof names[0]);
 
-// console.log(typeof [3][0]);
+console.log(typeof [3][0]);
 
-// console.log(typeof true);
+console.log(typeof true);
 
-// console.log(1 == 1); //true
-// console.log(1 != 2); //true
-// console.log('HELLO' == 2); //false
-// console.log('1' == 1); //true
-// console.log('1' === 1); //false
-// console.log(1 > 8); //false
-// console.log(1 <= 8) //true
-// console.log(1 <= 1); //true
+console.log(1 == 1); //true
+console.log(1 != 2); //true
+console.log('HELLO' == 2); //false
+console.log('1' == 1); //true
+console.log('1' === 1); //false
+console.log(1 > 8); //false
+console.log(1 <= 8) //true
+console.log(1 <= 1); //true
 
-// console.log(1 === 1 && 3 === 4);  //false
-// console.log((1 !== 1 || 3 === 4) || true );  //true
+console.log(1 === 1 && 3 === 4);  //false
+console.log((1 !== 1 || 3 === 4) || true );  //true
 
 let name = 'benjamin';
 name = 'benjamin-better';

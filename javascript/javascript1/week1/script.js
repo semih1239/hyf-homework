21d07857467d357d06fbf7d9d50f2daf44259610
semih1239 <semihayyildiz23@hotmail.com>
2021-05-05 17:57:12 +0200

    Fixed comment lines

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,57 +1,57 @@
 // Age-ify (A future age calculator)
 
-// let yearOfBirth = 1995
-// let yearFuture = 2055
+let yearOfBirth = 1995
+let yearFuture = 2055
 
-// let age = yearFuture - yearOfBirth
+let age = yearFuture - yearOfBirth
 
-// console.log(`You will be ${age} years old in ${yearFuture} `)
+console.log(`You will be ${age} years old in ${yearFuture} `)
 
 // // Goodboy-Oldboy (A dog age calculator)
 
-// let dogYearOfBirh = 2018
-// let dogYearFuture = 2027
+let dogYearOfBirh = 2018
+let dogYearFuture = 2027
 
-// let question = confirm('Do you want dog year?')
+let question = confirm('Do you want dog year?')
 
-// let dogYear = (dogYearFuture - dogYearOfBirh) * 7
-// let humanYear = (dogYearFuture - dogYearOfBirh)
+let dogYear = (dogYearFuture - dogYearOfBirh) * 7
+let humanYear = (dogYearFuture - dogYearOfBirh)
 
 
-// if (question){
-//     console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`)
-// }
-// else{
-//     console.log(`Your dog will be ${humanYear} human years old in ${dogYearFuture}`)
-// }
+if (question){
+    console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`)
+}
+else{
+    console.log(`Your dog will be ${humanYear} human years old in ${dogYearFuture}`)
+}
 
 // Housey pricey (A house price estimator)
 
-// let high = prompt('What is your home high?')
-// let wide = prompt('What is your home wide?')
-// let deep = prompt('What is your home deep?')
-// let gardenSize = prompt('What is your garden size (m2)')
+let high = prompt('What is your home high?')
+let wide = prompt('What is your home wide?')
+let deep = prompt('What is your home deep?')
+let gardenSize = prompt('What is your garden size (m2)')
 
-// let volumeInMeters = high * wide * deep
+let volumeInMeters = high * wide * deep
 
-// housePrice = (volumeInMeters * 2.5 * 1000) + (gardenSize * 300)
+housePrice = (volumeInMeters * 2.5 * 1000) + (gardenSize * 300)
 
-// console.log(`House price is ${housePrice}`)
+console.log(`House price is ${housePrice}`)
 
-// let ask = confirm('Would you like to compare with the money purchased?')
+let ask = confirm('Would you like to compare with the money purchased?')
 
-// if(ask){
-//     let compare = prompt('How much purschase money?')
-//         if(compare>housePrice){
-//             console.log(`You bought the house expensive. You gave the house ${compare-housePrice} more than necessary.`)
-//         }
-//         else if(compare==housePrice){
-//             console.log('You bought the house at normal price')
-//         }
-//         else{
-//             console.log(`You bought the house cheap. You gave the house ${housePrice-compare} lower than necessary.`)
-//         }
-//     }
+if(ask){
+    let compare = prompt('How much purschase money?')
+        if(compare>housePrice){
+            console.log(`You bought the house expensive. You gave the house ${compare-housePrice} more than necessary.`)
+        }
+        else if(compare==housePrice){
+            console.log('You bought the house at normal price')
+        }
+        else{
+            console.log(`You bought the house cheap. You gave the house ${housePrice-compare} lower than necessary.`)
+        }
+    }
 
 // Ez Namey (Startup name generator) Optional
 

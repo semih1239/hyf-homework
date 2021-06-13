8c303f1e50fcf5a9b59a3efbfd799e4b9ab5f823
Semih <semihayyildiz23@hotmail.com>
2021-06-13 04:16:07 +0300

    Added Javascript2 Week2 Homeworks

--- javascript/javascript2/week2/warmupArrayExercises.js
+++ javascript/javascript2/week2/warmupArrayExercises.js
@@ -1,8 +1,8 @@
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const doubledNumbers = numbers.filter(number => number % 2 != 0).map(number => number * 2)
+const doubledNumbers = numbers.filter(number => number % 2 != 0).map(number => number * 2)
 
-// console.log("The doubled numbers are", doubledNumbers)
+console.log("The doubled numbers are", doubledNumbers)
 
 
 

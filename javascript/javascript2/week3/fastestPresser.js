c199a3186569de3ba82802e0a329a670e3c4e03b
semih1239 <semihayyildiz23@hotmail.com>
2021-06-17 16:05:33 +0200

    Fixed

--- javascript/javascript2/week3/fastestPresser.js
+++ javascript/javascript2/week3/fastestPresser.js
@@ -10,39 +10,40 @@ const restartButton = document.getElementById('restartButton')
 const time = document.getElementById('seconds')
 
 
-function allSolition(){
-        keyS = 0
-        keyL = 0
-        countS.textContent = 0
-        countL.textContent = 0
-        function Count(event) {
-            if (event.key === 's') {
-                keyS++
-                countS.textContent = keyS
-            }
-            if (event.key === 'l') {
-                keyL++
-                countL.textContent = keyL
-            }
+function allSolition() {
+    keyS = 0
+    keyL = 0
+    countS.textContent = 0
+    countL.textContent = 0
+    function Count(event) {
+        if (event.key === 's') {
+            keyS++
+            countS.textContent = keyS
         }
-        window.addEventListener('keydown', Count)
-        function removeFunction() {
-            if (keyS > keyL) {
-                countS.textContent = 'Winner winner chicken dinner !!!'
-            }
-            else if (keyL > keyS) {
-                countL.textContent = 'Winner winner chicken dinner !!!'
-            }
-            else {
-                countS.textContent = 'Winner winner half chicken dinner !!!'
-                countL.textContent = 'Winner winner half chicken dinner !!!'
-            }
-            window.removeEventListener('keydown', Count)
+        if (event.key === 'l') {
+            keyL++
+            countL.textContent = keyL
         }
-        const timeWithSeconds = time.value * 1000
-    
-        setTimeout(removeFunction, timeWithSeconds)
+    }
+    window.addEventListener('keydown', Count)
+    function removeFunction() {
+        if (keyS > keyL) {
+            countS.textContent = 'Winner winner chicken dinner !!!'
+        }
+        else if (keyL > keyS) {
+            countL.textContent = 'Winner winner chicken dinner !!!'
+        }
+        else {
+            countS.textContent = 'Winner winner half chicken dinner !!!'
+            countL.textContent = 'Winner winner half chicken dinner !!!'
+        }
+        window.removeEventListener('keydown', Count)
+    }
+    const timeWithSeconds = time.value * 1000
+
+    setTimeout(removeFunction, timeWithSeconds)
 }
 
 startButton.addEventListener('click', allSolition)
 restartButton.addEventListener('click', allSolition)
+

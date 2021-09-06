fde8b05b93ba5378936229c89d78d565d50a9968
semih1239 <semihayyildiz23@hotmail.com>
2021-09-06 16:30:16 +0200

    Added react week1 homework

---
+++ react/week1/todo-list/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Todos from './App';
+import reportWebVitals from './reportWebVitals';
+import getTodos from './getTodos'
+
+const todos = getTodos()
+
+function Render(){
+  return (
+    <div>
+      {todos.map((todo) => {
+        return <Todos description={todo.description} deadline={todo.deadline} />
+      })}
+    </div>
+  )
+}
+
+ReactDOM.render(<Render />, document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
